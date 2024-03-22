48da08bb4f4cef210c2b961b996116c758da01d8
Jeremy Tollenaers <jtollena@mi-c2-p2.s19.be>
2024-03-22 15:25:51 +0100

    save

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jtollena <jtollena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 14:29:35 by jtollena          #+#    #+#             */
-/*   Updated: 2024/03/22 14:35:34 by jtollena         ###   ########.fr       */
+/*   Updated: 2024/03/22 15:18:25 by jtollena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,32 @@
 
 void	replace(std::string *str, std::string from, std::string to)
 {
-	std::string cpy = *str;
 	int startAt;
-	for(int i = 0; cpy[i] != 0; i++) {
-		int j = 0;
-		while (cpy[i] == from[j]){
+	for(int i = 0; (*str)[i] != 0; i++) {
+		unsigned long j = 0;
+		while ((*str)[i] == from[j]){
 			j++;
 			i++;
 		}
 		if (j == from.size()){
 			startAt = i - j;
-			cpy.erase(remove(startAt, j), j);
-			for(int k; k < sizeof(to); k++){
-				cpy[startAt + k] = to[k];
-				i += to.size();
-			}
+			(*str).erase(startAt, j);
+			(*str).insert(startAt, to);
+			i += to.size();
 		}
 	}
 }
+
+std::string charToString(std::string str){ return str; }
 
 int main(int argc, char **argv) {
 	if (argc != 4)
 		return (std::cout << "Please enter 3 argc; <file> <occurence to replace> <replace by>" << std::endl, 1);
 	std::ifstream file1(argv[1]);
 	if (file1.good()){
+		std::ofstream file2(charToString(argv[1]).append(".replace"));
+		if (!file2.good())
+			return (std::cout << "Error occured while trying to create output file." << std::endl, 1);
 		std::string str;
 		std::string line;
 		str.reserve(file1.tellg());
@@ -47,7 +49,7 @@ int main(int argc, char **argv) {
 			str.append("\n");
 		}
 		replace(&str, argv[2], argv[3]);
-		std::cout << str;
+		file2 << str;
 		file1.close();
 	} else return (std::cout << "File " << argv[1] << " do not exist" << std::endl, 1);
 	return 0;
